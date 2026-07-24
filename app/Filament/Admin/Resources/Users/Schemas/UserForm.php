648352d928b2a56c485e7570e73330b44d117a43
Mohamed Slimani <slimani.m1993@gmail.com>
2026-07-24 03:23:20 +0100

    feat: add user media import action

--- app/Filament/Admin/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Admin/Resources/Users/Schemas/UserForm.php
@@ -2,8 +2,11 @@
 
 namespace App\Filament\Admin\Resources\Users\Schemas;
 
+use CodeWithDennis\FilamentSelectTree\SelectTree;
+use Filament\Actions\Action;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Components\Group;
 use Filament\Schemas\Components\Section;
@@ -12,6 +15,8 @@ use Filament\Support\Icons\Heroicon;
 use Illuminate\Support\Facades\Hash;
 use Slimani\MediaManager\Form\MediaPicker;
 use Slimani\MediaManager\Form\RichEditor\MediaManagerRichContentPlugin;
+use Slimani\MediaManager\Models\File;
+use Slimani\MediaManager\Models\Folder;
 
 class UserForm
 {
@@ -69,7 +74,71 @@ class UserForm
                                 ->schema([
                                     MediaPicker::make('avatar_id')
                                         ->label('Avatar')
-                                        ->avatar(),
+                                        ->avatar()
+                                        ->hintActions([
+                                            Action::make('importImageFromUrl')
+                                                ->label('Import from URL')
+                                                ->icon(Heroicon::Link)
+                                                ->schema([
+                                                    TextInput::make('url')
+                                                        ->label('Image URL')
+                                                        ->url()
+                                                        ->required()
+                                                        ->placeholder('https://example.com/image.jpg'),
+                                                    SelectTree::make('folder_id')
+                                                        ->label('Destination folder')
+                                                        ->query(Folder::query()->orderBy('name'), 'name', 'parent_id')
+                                                        ->prepend([
+                                                            'name' => 'Root',
+                                                            'value' => 0,
+                                                        ])
+                                                        ->enableBranchNode()
+                                                        ->withCount()
+                                                        ->searchable(),
+                                                ])
+                                                ->action(function (MediaPicker $component, array $data): void {
+                                                    $file = File::create([
+                                                        'name' => pathinfo(parse_url($data['url'], PHP_URL_PATH) ?: 'image', PATHINFO_FILENAME) ?: 'image',
+                                                        'uploaded_by_user_id' => auth()->id(),
+                                                        'folder_id' => $data['folder_id'] ?? null,
+                                                    ]);
+
+                                                    try {
+                                                        $media = $file->addMediaFromUrl(
+                                                            $data['url'],
+                                                            'image/avif',
+                                                            'image/gif',
+                                                            'image/jpeg',
+                                                            'image/png',
+                                                            'image/webp',
+                                                        )->toMediaCollection('default', filament('media-manager')->getDisk());
+
+                                                        $file->update([
+                                                            'name' => pathinfo($media->file_name, PATHINFO_FILENAME),
+                                                            'size' => $media->size,
+                                                            'mime_type' => $media->mime_type,
+                                                            'extension' => $media->extension,
+                                                            'width' => $media->getCustomProperty('width'),
+                                                            'height' => $media->getCustomProperty('height'),
+                                                        ]);
+
+                                                        $component->state((string) $file->id);
+
+                                                        Notification::make()
+                                                            ->title('Image imported')
+                                                            ->success()
+                                                            ->send();
+                                                    } catch (\Throwable $exception) {
+                                                        $file->delete();
+
+                                                        Notification::make()
+                                                            ->title('Image could not be imported')
+                                                            ->body($exception->getMessage())
+                                                            ->danger()
+                                                            ->send();
+                                                    }
+                                                }),
+                                        ]),
                                     MediaPicker::make('cv_id')
                                         ->label('CV / Resume')
                                         ->directory('User/Documents')
